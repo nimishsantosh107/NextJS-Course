e83932b795be4a72155d159a3e1c76e708d7e70d
nimishsantosh107 <36289353+nimishsantosh107@users.noreply.github.com>
2022-09-22 23:04:38 +0530

    added getStaticProps

--- youtube-1-basics/pages/users/index.js
+++ youtube-1-basics/pages/users/index.js
@@ -1,10 +1,31 @@
 import Link from "next/link";
 
-const Users = ({}) => {
+export const getStaticProps = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+    return {
+        props: { users: data },
+    };
+};
+
+const Users = ({ users }) => {
+    console.log(users);
     return (
         <div>
-            Users
-            <Link href="/users/all">All Users</Link>
+            <h1>Users:</h1>
+
+            <div>
+                {users.map((user) => (
+                    <div key={user.id}>
+                        <h3>{user.name}</h3>
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <Link href="/users/all">All Users</Link>
+            </div>
         </div>
     );
 };
